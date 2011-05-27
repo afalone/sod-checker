c97ad29ceba0492a126efb172ecbe9a6cb6d0cda
afa <afa@afalone.local>
2011-05-27 12:38:08 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SodChecker::Application.config.secret_token = 'fe6556f05a90255d9af4fa0ec6d8600c301e6e3e48f4a1a6506746c3d2f9751351821c30454419d6be6d6f68ed7d8b06f9bc93fd98b8c5542222bf65a20aebfb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SodChecker::Application.config.session_store :cookie_store, :key => '_sod-checker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SodChecker::Application.config.session_store :active_record_store
