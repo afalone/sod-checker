4e480930043035bb80facebd714f6d4f07086ee8
afa <afa@afalone.local>
2011-07-11 11:40:45 +0400

    rm debug

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,8 +5,6 @@ SodChecker::Application.configure do
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
-  p config, config.class.name
-
   config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
